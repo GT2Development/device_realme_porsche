22c2d3bf5211f29c34df31acf045ccfae2962067
GT2Development <151126166+GT2Development@users.noreply.github.com>
2023-11-18 03:24:05 -0500

    drop low gamma mode

--- device.mk
+++ device.mk
@@ -30,8 +30,7 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630946728207028354.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946728207028354.xml
 
-PRODUCT_SYSTEM_PROPERTIES += \
-    persist.sys.brightness.low.gamma=true
+
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_$(DEVICE_SKU)/audio_effects.xml     
@@ -42,7 +41,6 @@ $(call inherit-product-if-exists, vendor/realme/porsche-opluscamera/opluscamera.
 # FaceSense
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.face.sense_service.camera_id=1
-
 # Thermal
 PRODUCT_COPY_FILES += \
 $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
